9d3027ca38d85e10ea525ea06bcd9a831184061b
Andrew Ealovega <Andrew@Ealovega.dev>
2024-03-20 13:02:27 -0400

    Fix tests

--- python_src/test/test_api.py
+++ python_src/test/test_api.py
@@ -3,14 +3,10 @@ from transferwareai.tccapi.api_cache import ApiCache
 
 
 class TestApi:
-    @pytest.fixture
-    def api_client(self, tmp_path):
-        api = ApiCache(tmp_path)
-        return api
 
     @pytest.mark.parametrize("num", (1, 2, 100))
-    def test_page(self, api_client, num):
+    def test_page(self, num):
         assert ApiCache.get_api_page(num) is not None
 
-    def test_end_page(self, api_client):
+    def test_end_page(self):
         assert ApiCache.get_api_page(999999999) == []
