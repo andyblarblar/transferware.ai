27843a77d10ef6ea2bb623922e09f7534f7c8553
maryamtello1 <maryamt@umich.edu>
2024-04-01 18:24:31 -0400

    added vector icons

--- static_site/transferware-app/src/pages/landing.js
+++ static_site/transferware-app/src/pages/landing.js
@@ -1,5 +1,11 @@
 import React from "react";
 import group3 from "../assets/images/Group-3.png";
+import upload from "../assets/images/upload-icon.png";
+import view from "../assets/images/view-icon.png";
+import database from "../assets/images/database-icon.png";
+
+
+
 
 function Landing() {
     return (
@@ -10,8 +16,8 @@ function Landing() {
           </h1>
         </div>
         <div className="flex flex-col lg:flex-row">
-          <div className="header-column flex flex-col pb-0 pt-10 px-16 lg:py-16 sm:px-20">
-            <div className="header lg:w-full">
+          <div className="header-column flex flex-col pb-0 pt-10 px-0 lg:py-16 sm:px-20">
+            <div className="header lg:w-full px-10 sm:px-0">
               <h1 className="font-bold text-3xl lg:text-6xl my-6">
                 Upload your sherd, Find your pattern
               </h1>
@@ -20,14 +26,23 @@ function Landing() {
                 ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna.
               </p>
-              <button className="bg-black text-white p-4 px-14 rounded-lg rounded-bl-none">
+              <button className="bg-black font-semibold text-white p-4 px-14 rounded-lg rounded-bl-none">
                 Use tool
               </button>
             </div>
-            <div className="flex flex-row space-x-10 my-20 font-semibold">
-              <span>Upload sherd</span>
-              <span>view 10 closest matches</span>
-              <span>get info from our database</span>
+            <div className="flex flex-row space-x-5 sm:space-x-10 my-20 font-semibold text-xs px-8 sm:px-0">
+              <span className="flex flex-row items-center">
+                <img src={upload} className="h-[20px] px-5" />
+                Upload sherd
+              </span>
+              <span className="flex flex-row items-center">
+                <img src={view} className="h-[20px] px-5" />
+                view 10 closest matches
+              </span>
+              <span className="flex flex-row items-center">
+                <img src={database} className="h-[20px] px-5" />
+                get info from our database
+              </span>
             </div>
           </div>
           <div className="flex flex-col justify-end items-end w-full lg:h-[calc(100vh-64px)]">
