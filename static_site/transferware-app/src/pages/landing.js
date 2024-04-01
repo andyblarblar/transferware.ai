8af5cf074fed0cd993a279ac94b8c87efe24062b
maryamtello1 <maryamt@umich.edu>
2024-04-01 14:59:20 -0400

    fixed height of header div

--- static_site/transferware-app/src/pages/landing.js
+++ static_site/transferware-app/src/pages/landing.js
@@ -11,34 +11,34 @@ import plate13 from "../assets/images/plate13.jpg";
 
 function Landing() {
     return (
-      <div>
+      <div className="h-screen">
         <div className="py-4 px-20">
-          <h1 className="text-2xl font-merriweather font-semibold">
+          <h1 className="text-2xl font-semibold">
             Transferware.<span className="text-blue-500">ai</span>
           </h1>
         </div>
-        <div className="flex flex-row p-6">
-          <div className="header-column flex flex-col sm:px-20">
-            <div className="header lg:w-2/4">
+        <div className="flex flex-row py-8 px-32 h-full">
+          <div className="header-column flex flex-col py-16">
+            <div className="header lg:w-3/4">
               <h1 className="font-bold text-6xl my-6">
                 Upload your sherd, Find your pattern
               </h1>
-              <p className="my-6">
+              <p className="my-8 w-4/5">
                 Explain how this tool is used blah blah blah ai blah. Lorem
                 ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna.
               </p>
-              <button className="bg-black text-white p-4 px-10 rounded-lg rounded-bl-none">
+              <button className="bg-black text-white p-4 px-14 rounded-lg rounded-bl-none">
                 Use tool
               </button>
             </div>
-            <div className="flex-row space-x-20 my-10 font-semibold">
+            <div className="flex-row space-x-20 my-20 font-semibold">
               <span>Upload sherd</span>
               <span>view 10 closest matches</span>
               <span>get info from our database</span>
             </div>
           </div>
-          <div class="grid grid-cols-4 grid-rows-3 gap-4 bg-white text-gray-700 w-1/2">
+          <div class="grid grid-cols-4 grid-rows-3 gap-4 bg-white text-gray-700 w-3/4 h-3/4">
             <div
               className="box col-span-2 row-span-1 bg-cover bg-center rounded-xl"
               style={{ backgroundImage: `url(${plate1})` }}
