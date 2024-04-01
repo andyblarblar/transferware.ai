184da8b20062e46bb3cb22f1cc64dd120edb2aa6
maryamtello1 <maryamt@umich.edu>
2024-04-01 09:20:29 -0400

    created header

--- static_site/transferware-app/src/pages/landing.js
+++ static_site/transferware-app/src/pages/landing.js
@@ -1,13 +1,36 @@
 import React from "react";
 
 function Landing() {
-    return(
-                <div className="bg-blue-500 text-white p-4">
-          <h1 className="text-2xl">Hello, Tailwind CSS!</h1>
-          <p className="text-lg">
-            This is a demonstration of Tailwind CSS in a React app.
-          </p>
+    return (
+      <div>
+        <div className="py-4 px-20">
+          <h1 className="text-2xl font-merriweather font-semibold">
+            Transferware.<span className="text-blue-500">ai</span>
+          </h1>
         </div>
+        <div className="flex-row p-6">
+          <div className="flex-col sm:px-20">
+            <div className="lg:w-2/5">
+              <h1 className="font-bold text-6xl my-6">
+                Upload your sherd, Find your pattern
+              </h1>
+              <p className="my-6">
+                Explain how this tool is used blah blah blah ai blah. Lorem
+                ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna.
+              </p>
+              <button className="bg-black text-white p-4 px-10 rounded-lg rounded-bl-none">
+                Use tool
+              </button>
+            </div>
+            <div className="flex-row space-x-20 my-10 font-semibold">
+              <span>Upload sherd</span>
+              <span>view 10 closest matches</span>
+              <span>get info from our database</span>
+            </div>
+          </div>
+        </div>
+      </div>
     );
 }
 export default Landing;
--- static_site/transferware-app/tailwind.config.js
+++ static_site/transferware-app/tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  //remove unused styles during build 
+  //remove unused styles during build
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        merriweather: ["Merriweather", "serif"],
+      },
+    },
   },
   variants: {
     extend: {},
