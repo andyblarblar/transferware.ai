f2a9ef45beddb767dc0ca05ee8e68ef086cd5349
maryamtello1 <maryamt@umich.edu>
2024-04-09 01:17:45 -0400

    added url submission

--- static_site/transferware-app/src/App.js
+++ static_site/transferware-app/src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Landing from "./pages/landing.js";
+import UploadPage from "./pages/uploadPage.js"
+
 function App() {
   return (
-    <Landing/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/uploadPage" element={<UploadPage />} />
+      </Routes>
+    </Router>
   );
 }
 
