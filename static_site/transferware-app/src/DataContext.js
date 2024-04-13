b6f3b3b34f78a43f930701e915f7c6368a3b1ceb
maryamtello1 <maryamt@umich.edu>
2024-04-13 13:46:48 -0400

    modified data context to store from local storage

--- static_site/transferware-app/src/DataContext.js
+++ static_site/transferware-app/src/DataContext.js
@@ -1,11 +1,18 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 
 const DataContext = createContext();
 
 export const useData = () => useContext(DataContext);
 
 export const DataProvider = ({ children }) => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState(() => {
+    const savedData = localStorage.getItem("data");
+    return savedData ? JSON.parse(savedData) : null;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(data));
+  }, [data]);
 
   return (
     <DataContext.Provider value={{ data, setData }}>
