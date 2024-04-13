10d87232e276a61ede672642ea0d68633bf0293a
maryamtello1 <maryamt@umich.edu>
2024-04-13 10:58:08 -0400

    added comments

--- static_site/transferware-app/src/components/matchResults.js
+++ static_site/transferware-app/src/components/matchResults.js
@@ -10,15 +10,18 @@ const MatchResults = () => {
     <div className="grid grid-cols-2 gap-4 p-4">
       {data.map((item) => (
         <div key={item.id} className="p-3 border rounded shadow-sm">
+            {/* pattern name attribute */}
           <p>
             <span className="font-semibold">Pattern name:</span>{" "}
             {item.pattern_name}
           </p>
+          {/* confidence attribute */}
           <p>
             <span className="font-semibold">Confidence:</span>{" "}
             {item.confidence.toFixed(4)}%
           </p>
           <p>
+            {/* pattern url attribute */}
             <span className="font-semibold">Pattern URL:</span>{" "}
             <a
               className="text-blue-600"
