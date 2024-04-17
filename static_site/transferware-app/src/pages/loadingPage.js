27ba44e04d824f1ed4bbf3b0b594ea487a435622
maryamtello1 <maryamt@umich.edu>
2024-04-17 13:35:20 -0400

    fixed typo (class to className)

--- static_site/transferware-app/src/pages/loadingPage.js
+++ static_site/transferware-app/src/pages/loadingPage.js
@@ -6,7 +6,7 @@ function LoadingAnimation(){
       <div className="flex flex-col justify-center items-center h-screen">
         <div className="flex flex-col justify-center items-center ">
           <img className="h-3/5 pb-8" src={archeologistDigging}></img>
-                  <div class="loader"></div>
+                  <div className="loader"></div>
         </div>
         </div>
     );
