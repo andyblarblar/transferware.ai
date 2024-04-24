49b2dee24a9b1b251a8c3e6ae3fd8b36acd41f4d
maryamtello1 <maryamt@umich.edu>
2024-04-24 00:28:19 -0400

    fixed upload page length

--- static_site/transferware-app/src/pages/uploadPage.js
+++ static_site/transferware-app/src/pages/uploadPage.js
@@ -150,7 +150,7 @@ function UploadPage() {
 
 
   return (
-    <div className="flex justify-center items-center w-full h-screen p-10">
+    <div className="flex justify-center items-center w-full  p-10">
       <div className="flex bg-white flex-col items-center md:w-3/5 w-full px-8 sm:px-20 py-4 rounded-xl shadow-[0px_20px_50px_0px_#A0AEC0]">
         <h1 className="w-full pb-5 text-center sm:text-start font-semibold text-xl">
           Upload a Photo of Your Sherd
