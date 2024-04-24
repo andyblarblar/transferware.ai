8cb0dd91904b883cf2421244a4a6f00e8af3d587
maryamtello1 <maryamt@umich.edu>
2024-04-24 02:23:21 -0400

    fixed font sizing on small screens

--- static_site/transferware-app/src/components/navbarAlternate.js
+++ static_site/transferware-app/src/components/navbarAlternate.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const NavbarAlternate = () => {
   return (
     <div className=" flex flex-row items-center justify-between text-white w-full fixed py-3 px-6 lg:px-8 ">
-      <Link to={"/"} className="text-2xl md:ml-12 font-semibold">
+      <Link to={"/"} className="text-lg md:text-2xl md:ml-12 font-semibold">
         Transferware.<span className="text-blue-500">ai</span>
       </Link>
       <ul className="flex font-semibold space-x-2 sm:space-x-8 sm:text-lg">
