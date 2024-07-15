417b6c4e433f74af5ea8f01488d5349fea16fbd9
maryamtello1 <maryamt@umich.edu>
2024-07-15 02:33:21 -0400

    modified navbar to not be transparent

--- static_site/transferware-app/src/components/navbarAlternate.js
+++ static_site/transferware-app/src/components/navbarAlternate.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const NavbarAlternate = () => {
   return (
-    <div className=" flex flex-row items-center justify-between w-full fixed z-10 py-3 px-6 lg:px-8 ">
+    <div className=" flex flex-row items-center justify-between bg-white w-full fixed z-10 py-3 px-6 lg:px-8 ">
       <Link to={"/"} className="text-lg md:text-2xl md:ml-12 font-semibold">
         Transferware.<span className="text-blue-500">ai</span>
       </Link>
