f30cc40f99b636dd143e78a92062700df36c25fd
maryamtello1 <maryamt@umich.edu>
2024-08-03 12:37:51 -0400

    added drag and drop functionality

--- static_site/transferware-app/src/pages/uploadPage.js
+++ static_site/transferware-app/src/pages/uploadPage.js
@@ -1,27 +1,28 @@
-import React, { useState, useRef} from "react";
-import {useLocation, useNavigate} from 'react-router-dom';
+import React, { useState, useRef } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import photoIcon from "../assets/images/photo-icon.png";
-import cross from "../assets/images/Cross.png";
 import { useData } from "../DataContext";
-import heic2any from "heic2any"; 
+import heic2any from "heic2any";
 import Footer from "../components/footer";
+
 function UploadPage() {
   const location = useLocation();
   const [selectedFile, setSelectedFile] = useState(null);
-  const [errorMessage, setErrorMessage] = useState((location.state || {errorMessage: ""}).errorMessage);
+  const [errorMessage, setErrorMessage] = useState(
+    (location.state || { errorMessage: "" }).errorMessage
+  );
   const [uploadedFileName, setUploadedFileName] = useState("");
   const [imageUrl, setImageUrl] = useState("");
-  const [imagePreviewUrl, setImagePreviewUrl] = useState(null); // State to hold the preview URL
+  const [imagePreviewUrl, setImagePreviewUrl] = useState(null);
   const [fileSize, setFileSize] = useState("");
-  const fileInputRef = useRef(null); // Added useRef to create a reference to the file input
+  const fileInputRef = useRef(null);
   const { setData } = useData();
   const navigate = useNavigate();
 
-  // Base url for the query api
+  // Base url for the query API
   const base_url = process.env.REACT_APP_QUERY_BASE;
 
-  const handleFileChange = async (e) => {
-    const file = e.target.files[0];
+  const handleFileChange = async (file) => {
     if (
       file &&
       (file.type === "image/png" ||
@@ -29,6 +30,7 @@ function UploadPage() {
         file.type === "image/heic")
     ) {
       let processedFile = file;
+
       // Convert HEIC to JPEG if needed
       if (file.type === "image/heic") {
         try {
@@ -46,21 +48,32 @@ function UploadPage() {
           return;
         }
       }
+
       setSelectedFile(processedFile);
       setErrorMessage("");
       setUploadedFileName(processedFile.name);
-      setImagePreviewUrl(URL.createObjectURL(processedFile)); // Create and set the image URL for preview
+      setImagePreviewUrl(URL.createObjectURL(processedFile));
 
       // Calculate file size in kilobytes
       const fileSizeInKB = Math.round((processedFile.size / 1024) * 100) / 100;
       setFileSize(fileSizeInKB + " KB");
     } else {
       setSelectedFile(null);
-      setUploadedFileName(""); // Clear uploaded file name
-      setFileSize(""); // Clear file size
-      setImagePreviewUrl(null); // Clear the image preview URL
+      setUploadedFileName("");
+      setFileSize("");
+      setImagePreviewUrl(null);
       setErrorMessage("Please select a valid PNG, JPG, or HEIC file.");
     }
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    handleFileChange(file);
   };
 
   const handleUrlChange = (e) => {
@@ -105,7 +118,7 @@ function UploadPage() {
     setSelectedFile(null);
     setUploadedFileName("");
     setFileSize("");
-    setImagePreviewUrl(null); // Clear the image preview URL
+    setImagePreviewUrl(null);
     if (fileInputRef.current) fileInputRef.current.value = "";
   };
 
@@ -161,27 +174,29 @@ function UploadPage() {
         const finalResults = await Promise.all(patternImagePromises);
         setData(finalResults);
 
-        // Use replace here to navigate to viewMatches
         navigate("/viewMatches", {
           replace: true,
           state: { imagePreviewUrl: imagePreviewUrl },
         });
       } catch (error) {
-        navigate("/uploadPage", {state: {errorMessage: `Failed to submit the file.`}})
+        navigate("/uploadPage", { state: { errorMessage: "Failed to submit the file." } });
         console.error("Error fetching data:", error);
       }
     }
   };
 
-
   return (
     <div>
       <div className="flex justify-center items-center w-full p-10 h-screen pt-16">
-        <div className="flex bg-white  flex-col items-center md:w-3/5 w-full px-8 sm:px-20 py-4 rounded-xl border-2 border-slate-400 shadow-[5px_5px_rgba(100,_116,_139,_0.4),_10px_10px_rgba(100,_116,_139,_0.3),_15px_15px_rgba(100,_116,_139,_0.2),_20px_20px_rgba(100,_116,_139,_0.1),_25px_25px_rgba(100,_116,_139,_0.05)]">
+        <div className="flex bg-white flex-col items-center md:w-3/5 w-full px-8 sm:px-20 py-4 rounded-xl border-2 border-slate-400 shadow-[5px_5px_rgba(100,_116,_139,_0.4),_10px_10px_rgba(100,_116,_139,_0.3),_15px_15px_rgba(100,_116,_139,_0.2),_20px_20px_rgba(100,_116,_139,_0.1),_25px_25px_rgba(100,_116,_139,_0.05)]">
           <h1 className="w-full pb-5 text-center sm:text-start font-semibold text-xl">
             Upload a Photo of Your Sherd
           </h1>
-          <div className="w-full h-1/3 flex flex-col items-center justify-center border-2 border-slate-300 border-dashed rounded-md py-8 lg:py-16 px-6">
+          <div
+            className="w-full h-1/3 flex flex-col items-center justify-center border-2 border-slate-300 border-dashed rounded-md py-8 lg:py-16 px-6"
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+          >
             <img src={photoIcon} className="max-h-28" alt="photo-icon" />
             <h2 className="text-center font-medium py-0">
               Drop your image here, or
@@ -197,18 +212,19 @@ function UploadPage() {
                 id="fileInput"
                 type="file"
                 accept=".png,.jpg,.jpeg,.heic"
-                onChange={handleFileChange}
+                onChange={(e) => handleFileChange(e.target.files[0])}
                 className="hidden"
               />
             </h2>
             <p className="text-neutral-400 text-xs py-2 sm:py-0">
-              Supports: PNG and JPG images
+              Supports: PNG, JPG, and HEIC images
             </p>
             {errorMessage && <p className="text-red-500">{errorMessage}</p>}
           </div>
-          {/* uploaded file name & size display */}
+
+          {/* Uploaded file name & size display */}
           <div
-            className={`w-full flex flex-row items-center border-2 rounded-lg px-4 sm:px-8 py-1 mt-6   ${
+            className={`w-full flex flex-row items-center border-2 rounded-lg px-4 sm:px-8 py-1 mt-6 ${
               !uploadedFileName && "hidden"
             }`}
           >
@@ -217,13 +233,11 @@ function UploadPage() {
               alt="Preview picture"
               className="max-h-16 rounded-lg"
             />
-
             {uploadedFileName && (
-              <p className=" w-full px-2 sm:px-8 py-4 flex flex-col justify-between break-words break-all text-xs font-semibold text-blue-900">
+              <p className="w-full px-2 sm:px-8 py-4 flex flex-col justify-between break-words break-all text-xs font-semibold text-blue-900">
                 {uploadedFileName}
                 <span className="pt-2 text-zinc-400 font-semibold">
                   ({fileSize})
-                  {/* <img src={cross} className="" alt="photo-icon" /> */}
                 </span>
               </p>
             )}
